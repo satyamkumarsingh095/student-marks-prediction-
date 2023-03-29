fd3ac736d260cdd9d4e1b3a8c56a0d6a661f8dc8
Satyam Kumar Singh <116185600+satyamkumarsingh095@users.noreply.github.com>
2023-03-29 20:10:48 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import pandas as pd
+from flask import Flask, request, render_template
+import joblib
+
+app = Flask(__name__)
+
+model = joblib.load("Students_marks_predictor_model.pkl")
+
+df = pd.DataFrame()
+
+@app.route('/')
+def home():
+    return render_template('satyam.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    global df
+    
+    input_features = [int(x) for x in request.form.values()]
+    features_value = np.array(input_features)
+    
+    #validate input hours
+    if input_features[0] <0 or input_features[0] >24:
+        return render_template('satyam.html', prediction_text='Please enter valid hours between 1 to 24 if you live on the Earth 😂')
+        
+
+    output = model.predict([features_value])[0][0].round(2)
+
+    # input and predicted value store in df then save in csv file
+    df= pd.concat([df,pd.DataFrame({'Study Hours':input_features,'Predicted Output':[output]})],ignore_index=True)
+    print(df)   
+    df.to_csv('smp_data_from_app.csv')
+
+    return render_template('satyam.html', prediction_text='You will get [{}%] marks, when you do study [{}] hours per day '.format(output, int(features_value[0])))
+
+
+if __name__ == "__main__":
+    app.run()
